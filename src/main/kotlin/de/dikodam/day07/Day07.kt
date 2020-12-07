74911bf4cd6b365aa27275e32862fe5beeef4257
dikodam <adiko@gmx.de>
2020-12-07 11:32:07 +0100

    Day07 clarified input grammar

---
+++ src/main/kotlin/de/dikodam/day07/Day07.kt
@@ -0,0 +1,22 @@
+package de.dikodam.day07
+
+
+fun main() {
+    // AMOUNT_DEF := "1 COLOR bag" OR "AMOUNT COLOR bags"
+
+    //x contains none: <COLOR contain no other bags.>
+    // "faded blue bags contain no other bags."
+
+    //x contains 1 colors: <COLOR + " contain AMOUNT_DEF.>
+    // "bright white bags contain 1 shiny gold bag."
+
+    //x contains 1+ colors: <COLOR contain AMOUNT AMOUNT_DEF(, AMOUNT_DEF)*. >
+    //"dark orange bags contain 3 bright white bags, 4 muted yellow bags."
+
+}
+
+
+class Day07 {
+
+
+}
